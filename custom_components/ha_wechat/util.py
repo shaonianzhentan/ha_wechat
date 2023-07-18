af34312743d9ef6578fc1d48e3ebd5a39b1dccb3
shaonianzhentan <ilovevar@gmail.com>
2023-07-18 11:54:15 +0800

    update docs

--- custom_components/ha_wechat/util.py
+++ custom_components/ha_wechat/util.py
@@ -4,10 +4,6 @@ async def async_generate_qrcode(hass, topic, token):
     key = f'ha:{token}#{topic}'
     qrc = urllib.parse.quote(key)
     await hass.services.async_call('persistent_notification', 'create', {
-                'title': '使用【HomeAssistant家庭助理】小程序扫码关联',
-                'message': f'[![qrcode](https://cdn.dotmaui.com/qrc/?t={qrc})](https://github.com/shaonianzhentan/ha_wechat) <font size="6">内含密钥和订阅主题<br/>请勿截图分享</font>'
-            })
-    await hass.services.async_call('persistent_notification', 'create', {
-                'title': '发送以下数据到【HomeAssistant家庭助理】公众号关联',
-                'message': key
+                'title': '关联控制',
+                'message': f'## 小程序扫码关联 <br/> ![qrcode](https://cdn.dotmaui.com/qrc/?t={qrc}) <br/> 内含密钥和订阅主题, 请勿截图分享 <br/> <hr/> <h1>微信公众号关联</h1> 发送以下数据到【<b>HomeAssistant家庭助理</b>】公众号 <br/><br/> <mark>{key}</mark>'
             })
