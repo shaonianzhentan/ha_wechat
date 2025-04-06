3600e3405dccceb32fff608aa3cb9883bdecec0a
shaonianzhentan <ilovevar@gmail.com>
2025-04-06 23:24:47 +0800

    更新文档

--- custom_components/ha_wechat/util.py
+++ custom_components/ha_wechat/util.py
@@ -1,4 +1,3 @@
-import urllib
 import qrcode
 from io import BytesIO
 import base64
@@ -7,7 +6,6 @@ async def async_generate_qrcode(hass, topic, token):
     """生成二维码并显示在通知中"""
     # 构造二维码内容
     key = f'ha:{token}#{topic}'
-    qrc = urllib.parse.quote(key)
 
     # 生成二维码图片
     qr = qrcode.QRCode(
@@ -28,5 +26,5 @@ async def async_generate_qrcode(hass, topic, token):
     # 在通知中显示二维码
     await hass.services.async_call('persistent_notification', 'create', {
         'title': '小程序扫码关联',
-        'message': f'![qrcode](data:image/png;base64,{img_base64}) <br/> 内含密钥和订阅主题, 请勿截图分享'
+        'message': f'![qrcode](data:image/png;base64,{img_base64}) <br/> 内含密钥和订阅主题, 请勿截图分享 <br/> {key}'
     })
